ce709507fdea30fa7e345927df625b46c20ccd17
Victoria Guarnieri <vguarnieri1@hotmail.com>
2021-10-19 15:49:20 -0300

    Agregue los forms

---
+++ LP2 TP2021 - Guarnieri - Velloso/Carátula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LP2_TP2021___Guarnieri___Velloso
+{
+    public partial class Carátula : Form
+    {
+        public Carátula()
+        {
+            InitializeComponent();
+        }
+    }
+}
