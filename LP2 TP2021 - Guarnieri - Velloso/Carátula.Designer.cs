26218fedde1e6cdba457b501e799e0d9e2ed8e3b
Manu Velloso <manuvelloso2@gmail.com>
2021-10-19 16:22:26 -0300

    forms

--- LP2 TP2021 - Guarnieri - Velloso/Carátula.Designer.cs
+++ LP2 TP2021 - Guarnieri - Velloso/Carátula.Designer.cs
@@ -31,26 +31,57 @@ namespace LP2_TP2021___Guarnieri___Velloso
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Titulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::LP2_TP2021___Guarnieri___Velloso.Properties.Resources.FONDO;
-            this.pictureBox1.Location = new System.Drawing.Point(109, 20);
+            this.pictureBox1.Image = global::LP2_TP2021___Guarnieri___Velloso.Properties.Resources.FONDO1;
+            this.pictureBox1.Location = new System.Drawing.Point(116, 32);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(568, 418);
+            this.pictureBox1.Size = new System.Drawing.Size(555, 418);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Titulo
             // 
             this.Titulo.AutoSize = true;
-            this.Titulo.Location = new System.Drawing.Point(307, 20);
+            this.Titulo.Font = new System.Drawing.Font("Sitka Banner", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Titulo.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Titulo.Location = new System.Drawing.Point(72, 9);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(177, 15);
+            this.Titulo.Size = new System.Drawing.Size(440, 47);
             this.Titulo.TabIndex = 2;
             this.Titulo.Text = "Cobertura del tablero de Ajedrez";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(677, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(123, 450);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(110, 450);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Titulo);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(110, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(567, 76);
+            this.panel3.TabIndex = 5;
             // 
             // Carátula
             // 
@@ -58,14 +89,17 @@ namespace LP2_TP2021___Guarnieri___Velloso
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "Carátula";
             this.Text = "Carátula";
             this.Load += new System.EventHandler(this.Carátula_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -73,5 +107,8 @@ namespace LP2_TP2021___Guarnieri___Velloso
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
