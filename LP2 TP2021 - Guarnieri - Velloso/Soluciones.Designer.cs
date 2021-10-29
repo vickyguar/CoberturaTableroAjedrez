c375af57eebeebd72cb26ef5529c4a1268c0548b
Manu Velloso <manuvelloso2@gmail.com>
2021-10-29 12:42:13 -0300

    Imagenes, Posicion ficha, algo del forms

--- LP2 TP2021 - Guarnieri - Velloso/Soluciones.Designer.cs
+++ LP2 TP2021 - Guarnieri - Velloso/Soluciones.Designer.cs
@@ -29,31 +29,65 @@ namespace LP2_TP2021___Guarnieri___Velloso
         /// </summary>
         private void InitializeComponent()
         {
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // vScrollBar1
+            // dataGridView1
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(777, 0);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(14, 441);
-            this.vScrollBar1.TabIndex = 0;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(189, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(425, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_next
+            // 
+            this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_next.Location = new System.Drawing.Point(667, 216);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            // 
+            // btn_back
+            // 
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Location = new System.Drawing.Point(52, 216);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(75, 23);
+            this.btn_back.TabIndex = 2;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = true;
             // 
             // Soluciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Soluciones";
             this.Text = "Soluciones";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_back;
     }
 }
 
